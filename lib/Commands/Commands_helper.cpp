cf1aab933f45a165d3ef424bd6143bc545bdf378
Björn Reimer <git@bjrm.de>
2019-08-22 09:08:24 +0200

    ping_pong_sin & ping_pong_cos

--- lib/Commands/Commands_helper.cpp
+++ lib/Commands/Commands_helper.cpp
@@ -15,6 +15,16 @@ float Commands::ping_pong_linear(uint8_t period_100ms, uint8_t offset_100ms)
   }
 }
 
+float Commands::ping_pong_sin(uint8_t period_100ms, uint8_t offset_100ms)
+{
+  return (sinf(float(millis() - offset_100ms * 100) * 2.0 * PI / float(period_100ms * 100)) + 1.0) / 2.0;
+}
+
+float Commands::ping_pong_cos(uint8_t period_100ms, uint8_t offset_100ms)
+{
+  return (cosf(float(millis() - offset_100ms * 100) * 2.0 * PI / float(period_100ms * 100)) + 1.0) / 2.0;
+}
+
 float Commands::ease_in_cubic(float t)
 {
   return t * t * t;
