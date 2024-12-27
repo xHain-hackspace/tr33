acb0288e7f2193a9fa243985b0192f46af831d7d
DeployKeyUser <154696240+arglurgl-machine@users.noreply.github.com>
2024-12-27 14:51:30 +0100

    update garderobe pins

--- lib/Garderobe/Garderobe.cpp
+++ lib/Garderobe/Garderobe.cpp
@@ -3,14 +3,16 @@
 
 Garderobe::Garderobe()
 {
-  FastLED.addLeds<NEOPIXEL, STRIP_PIN_1>(leds[0], STRIP_PIXEL_COUNT);
-  FastLED.addLeds<NEOPIXEL, STRIP_PIN_2>(leds[1], STRIP_PIXEL_COUNT);
-  FastLED.addLeds<NEOPIXEL, STRIP_PIN_3>(leds[2], STRIP_PIXEL_COUNT);
-  FastLED.addLeds<NEOPIXEL, STRIP_PIN_4>(leds[3], STRIP_PIXEL_COUNT);
-  FastLED.addLeds<NEOPIXEL, STRIP_PIN_5>(leds[4], STRIP_PIXEL_COUNT);
-  FastLED.addLeds<NEOPIXEL, STRIP_PIN_6>(leds[5], STRIP_PIXEL_COUNT);
-  FastLED.addLeds<NEOPIXEL, STRIP_PIN_7>(leds[6], STRIP_PIXEL_COUNT);
-  FastLED.addLeds<NEOPIXEL, STRIP_PIN_8>(leds[7], STRIP_PIXEL_COUNT);
-  FastLED.addLeds<NEOPIXEL, STRIP_PIN_9>(leds[8], STRIP_PIXEL_COUNT);
-  FastLED.addLeds<NEOPIXEL, STRIP_PIN_10>(leds[9], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_1>(LedStructure::leds[0], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_2>(LedStructure::leds[1], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_3>(LedStructure::leds[2], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_4>(LedStructure::leds[3], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_5>(LedStructure::leds[4], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_6>(LedStructure::leds[5], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_7>(LedStructure::leds[6], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_8>(LedStructure::leds[7], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_9>(LedStructure::leds[8], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_10>(LedStructure::leds[9], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_11>(LedStructure::leds[10], STRIP_PIXEL_COUNT);
+  FastLED.addLeds<NEOPIXEL, STRIP_PIN_12>(LedStructure::leds[11], STRIP_PIXEL_COUNT);
 }
--- lib/Garderobe/Garderobe_config.h
+++ lib/Garderobe/Garderobe_config.h
@@ -1,17 +1,18 @@
 // strip config
-#define STRIP_PIN_1 33
-#define STRIP_PIN_2 26
-#define STRIP_PIN_3 25
+#define STRIP_PIN_1 15
+#define STRIP_PIN_2 13
+#define STRIP_PIN_3 14
 #define STRIP_PIN_4 27
-#define STRIP_PIN_5 14
-#define STRIP_PIN_6 23
-#define STRIP_PIN_7 22
-#define STRIP_PIN_8 21
-#define STRIP_PIN_9 19
-#define STRIP_PIN_10 5
-
-#define STRIP_PIXEL_COUNT 247
-#define STRIP_COUNT 10
+#define STRIP_PIN_5 25
+#define STRIP_PIN_6 26
+#define STRIP_PIN_7 32
+#define STRIP_PIN_8 33
+#define STRIP_PIN_9 4
+#define STRIP_PIN_10 12
+#define STRIP_PIN_11 18
+#define STRIP_PIN_12 5
+#define STRIP_PIXEL_COUNT 118
+#define STRIP_COUNT 12
 
 // mapping
 #define MAPPING_X_MIN 66.12582397460938
